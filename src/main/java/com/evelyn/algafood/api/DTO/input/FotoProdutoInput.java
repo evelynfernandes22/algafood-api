a5196c66cebbc180fb26376cc1a299ed9801cc3f
Evelyn Fernandes <evelyn.af22@gmail.com>
2023-08-19 20:32:37 -0300

    RestauranteProdutoFotoController

---
+++ src/main/java/com/evelyn/algafood/api/DTO/input/FotoProdutoInput.java
@@ -0,0 +1,14 @@
+package com.evelyn.algafood.api.DTO.input;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class FotoProdutoInput {
+
+	private MultipartFile arquivo;
+	private String descricao;
+}
---
+++ src/main/java/com/evelyn/algafood/api/controller/RestauranteProdutoFotoController.java
@@ -0,0 +1,37 @@
+package com.evelyn.algafood.api.controller;
+
+import java.nio.file.Path;
+import java.util.UUID;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.evelyn.algafood.api.DTO.input.FotoProdutoInput;
+
+@RestController
+@RequestMapping(value = "/restaurantes/{restauranteId}/produtos/{produtoId}/foto")
+public class RestauranteProdutoFotoController {
+
+	
+	@PutMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+	public void atualizarFoto(@PathVariable Long restauranteId, @PathVariable Long produtoId, 
+			FotoProdutoInput fotoProdutoInput) {
+		var nomeArquivo = UUID.randomUUID().toString() + "_" + fotoProdutoInput.getArquivo().getOriginalFilename();
+		
+		//Onde quero salvar o arquivo
+		var arquivoFoto = Path.of("\\Users\\evely\\OneDrive\\Imagens\\catalogo", nomeArquivo); 
+		
+		System.out.println(fotoProdutoInput.getDescricao());
+		System.out.println(arquivoFoto);
+		System.out.println(fotoProdutoInput.getArquivo().getContentType());
+		
+		try {
+			fotoProdutoInput.getArquivo().transferTo(arquivoFoto);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+}
